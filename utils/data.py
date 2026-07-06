1a692cc85fa4b4a140bc4c65dbe7f55867515271
Edsouzaa <edsonhenriqueabreu@gmail.com>
2026-07-06 12:41:16 -0300

    Atualização do data.py, agora ele salva informações das ações de tal bolsa

--- utils/data.py
+++ utils/data.py
@@ -3,13 +3,29 @@ import yfinance as yf
 # O que é yfinance? é uma biblioteca que permite buscar dados hístoricos de mercado,
 # sem precisar utilizar chaves de API.
 
+# Importa o módulo os para manipulação de arquivos e pastas
+import os
+
 # ticker nesse caso é utilizado para busca do historico de determinada bolsa.
 def get_stock(ticker):
+    
     # Seta o ticker como um objeto, contendo suas informações
     ticker_obj = yf.Ticker(ticker)
+    
     # Define o historico da ação baseado no period, neste caso 5 anos(5 years)
     data = ticker_obj.history(period="5y")
     
+    # Tratamento de excessão: Caso data esteja VAZIO o codigo devolve um aviso.
     if data.empty:
         print(f"AVISO: nenhum dado retornado para {ticker}")
+        return data
+    
+    # Cria a pasta "data" caso ela não exista
+    os.makedirs("data", exist_ok=True)
+
+    # Salva o histórico em um arquivo CSV
+    data.to_csv(f"data/{ticker}_historico.csv")
+
+    print(f"Histórico salvo em data/{ticker}_historico.csv")
+    
     return data
